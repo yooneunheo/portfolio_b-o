3d944a44f8811a0a8e899be943084dc82de4827e
yoonheo0419@gmail.com <yoonheo0419@gmail.com>
2021-01-02 15:35:18 +0900

    전반적인 코드 다듬기

--- js/sub-page.js
+++ js/sub-page.js
@@ -1,79 +1,73 @@
-'use strict';
+"use strict";
 
 (() => {
-    // EXPRESSION
-    let yOffset = 0; // window.pageYOffset
-    const filterCate = document.querySelector('.filter-category');
-    const itemWrapper = document.querySelector('.product-item-wrapper');
-    const productItem = document.querySelectorAll(".product-item");
-    const headerHeight = document.querySelector('#header').getBoundingClientRect().height;
-    const categoryBtn = document.querySelector('.category-btn');
+  // EXPRESSION
+  let yOffset = 0; // window.pageYOffset
+  const filterCategory = document.querySelector(".filter-category");
+  const itemWrapper = document.querySelector(".product-item-wrapper");
+  const productItem = document.querySelectorAll(".product-item");
+  const globalNavHeight = document
+    .querySelector(".global-nav")
+    .getBoundingClientRect().height;
+  const categoryBtn = document.querySelector(".category-btn");
 
-
-    // FUNCTION FOR STICKY NAV
-	function stickyCate() {
-		if (yOffset > headerHeight) {
-			document.body.classList.add('make-nav-sticky');
-		} else {
-			document.body.classList.remove('make-nav-sticky');
-		}
+  // FUNCTION FOR STICKY NAV
+  function stickyNav() {
+    if (yOffset > globalNavHeight) {
+      document.body.classList.add("sticky-nav");
+    } else {
+      document.body.classList.remove("sticky-nav");
     }
-    
+  }
 
-    // FUNCTION FOR ARROW UP BUTTON
-    function showArrowUp() {
-        const arrowUp = document.querySelector('.arrow-up');
+  // FUNCTION FOR ARROW UP BUTTON
+  function showArrowUp() {
+    const arrowUp = document.querySelector(".arrow-up");
 
-        if(yOffset > headerHeight) {
-            arrowUp.classList.add('visible');
+    if (yOffset > globalNavHeight) {
+      arrowUp.classList.add("visible");
+    } else {
+      arrowUp.classList.remove("visible");
+    }
+    arrowUp.addEventListener("click", () => {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    });
+  }
+
+  // SCROLL EVENT
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    stickyNav();
+    showArrowUp();
+  });
+
+  // RESPONSIVE NAV FOR SMALL WIDTH
+  categoryBtn.addEventListener("click", () => {
+    filterCategory.classList.toggle("show");
+  });
+
+  // FUNCTION FOR FILTERING
+  filterCategory.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter;
+    if (filter == null) {
+      return;
+    }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector(".filter-category__link.selected");
+    active.classList.remove("selected");
+    e.target.classList.add("selected");
+
+    itemWrapper.classList.add("ani-out");
+    setTimeout(() => {
+      productItem.forEach((item) => {
+        if (filter === "*" || item.dataset.type.includes(filter)) {
+          item.classList.remove("invisible");
         } else {
-            arrowUp.classList.remove('visible');
+          item.classList.add("invisible");
         }
-        arrowUp.addEventListener('click', () => {
-            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-        });
-    }
-
-
-    // SCROLL EVENT
-    window.addEventListener('scroll', () => {
-        yOffset = window.pageYOffset;
-        stickyCate();
-        showArrowUp();
-    });
-
-
-    // RESPONSIVE NAV FOR SMALL WIDTH
-    categoryBtn.addEventListener('click', () => {
-        filterCate.classList.toggle("show");
-    });
-
-
-    // FUNCTION FOR FILTERING
-    filterCate.addEventListener('click', (e)=>{
-        const filter = e.target.dataset.filter;
-        if(filter == null) {
-            return;
-        }
-
-        // Remove selection from the previous item and select the new one
-        const active = document.querySelector('.filter-category__link.selected');
-        active.classList.remove('selected');
-        e.target.classList.add('selected');
-
-
-        itemWrapper.classList.add('ani-out');
-        setTimeout(() => {
-            productItem.forEach((item) => {
-                if (filter === '*' || item.dataset.type.includes(filter)) {
-                    item.classList.remove('invisible');
-                } else {
-                    item.classList.add('invisible');
-                }
-            });
-            itemWrapper.classList.remove('ani-out');
-        }, 300);
-    });
-
+      });
+      itemWrapper.classList.remove("ani-out");
+    }, 300);
+  });
 })();
-
