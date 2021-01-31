8588667cd6380d101dda033bfe936253200be39a
yoonheo0419@gmail.com <yoonheo0419@gmail.com>
2021-02-01 04:26:11 +0900

    HTML, CSS, JS: 로그인폼 유효성 체크 기능 추가

--- js/login.js
+++ js/login.js
@@ -5,23 +5,48 @@
   const password = document.querySelector("#password");
   const btn = document.querySelector("#btn");
 
-  btn.addEventListener("click", () => {
-    if (email.value === "" || email.value == null) {
-      email.nextElementSibling.classList.add("warning");
-      email.nextElementSibling.nextElementSibling.classList.add("warning");
-      setTimeout(() => {
-        email.nextElementSibling.classList.remove("warning");
-        email.nextElementSibling.nextElementSibling.classList.remove("warning");
-      }, 1500);
-    } else if (password.value === "" || password.value == null) {
-      password.nextElementSibling.classList.add("warning");
-      password.nextElementSibling.nextElementSibling.classList.add("warning");
-      setTimeout(() => {
-        password.nextElementSibling.classList.remove("warning");
-        password.nextElementSibling.nextElementSibling.classList.remove(
-          "warning",
-        );
-      }, 1500);
-    }
+  // 즉각적인 반응을 위해 form의 submit이 아닌 btn의 click 이벤트로 처리
+  btn.addEventListener("click", (e) => {
+    checkInputs();
   });
+
+  function checkInputs() {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === "" || emailValue == null) {
+      setErrorFor(email, "Email cannot be blank");
+    } else if (!isEmail(emailValue)) {
+      setErrorFor(email, "Email is not valid");
+    } else {
+      setSucessFor(email);
+    }
+
+    if (passwordValue === "" || passwordValue == null) {
+      setErrorFor(password, "Password cannot be blank");
+    } else if (passwordValue.length <= 6) {
+      setErrorFor(password, "Password must be longer than 6 characters");
+    } else {
+      setSucessFor(password);
+    }
+  }
+
+  function setErrorFor(input, message) {
+    const inputArea = input.parentElement; // .input-area
+    const small = inputArea.querySelector("small");
+
+    inputArea.className = "input-area error";
+    small.innerText = message;
+  }
+
+  function setSucessFor(input) {
+    const inputArea = input.parentElement;
+    inputArea.className = "input-area success";
+  }
+
+  function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email,
+    );
+  }
 })();
